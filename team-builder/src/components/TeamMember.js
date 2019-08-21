def7dcf9c29e1e233cbd7abfdac888a000ab75b5
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-21 16:00:58 -0700

    card - may not use

--- team-builder/src/components/TeamMember.js
+++ team-builder/src/components/TeamMember.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TeamMember = (props) => {
 
     return(
-        <div className='tm'>
+        <div className='tm' key={props.id}>
             <h1>Name: {props.name} </h1>
             <h2>Email: {props.email} </h2>
             <h2>Role: {props.role} </h2>
