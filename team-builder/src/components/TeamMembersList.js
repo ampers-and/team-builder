c9cca2b142c4c87ab5b199fe3632084dd10df994
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-21 15:10:39 -0700

    file fixes

---
+++ team-builder/src/components/TeamMembersList.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect } from 'react'
+
+const TeamMembersList = (props) =>{
+
+    const [tMList, setTMList] = useState([]);
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default TeamMembersList;
