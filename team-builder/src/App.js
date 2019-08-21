5d03e83ce2e54b90499d6e37b94793ed9082d5ad
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-21 15:48:26 -0700

    app and form done-ish

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
 import './App.css';
 import TeamMembersList from './components/TeamMembersList'
+import Form from './components/Form';
 
 function App() {
+
+  const [teamMemebers, setTeamMemmbers] = useState({name:'Harry Potter', email:'hedwig@hogwarts.edu', role:'Auror'});
+  
+  const addNewTeamMember = teamMember =>{
+    setTeamMemmbers([...teamMemebers, teamMember]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         Team Builder
       </header>
-      <TeamMembersList/>
+      <Form addNewTeamMember={addNewTeamMember} />
+      <TeamMembersList TeamMembersList={teamMemebers} />
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const Form = props => {
+
+    const [teamMember, setTeamMember] = useState({name:'', email:'', role:''});
+    
+    const changeHandler = event => {
+        setTeamMember(event.target.value);
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+        const newTeamMember = {
+          ...teamMember,
+          id: Date.now()
+        };
+        props.addNewTeamMember(newTeamMember);
+        setTeamMember({ name: '', email:'', role:''});
+      };
+
+    return (
+        <form onSubmit={submitForm}>
+
+            <label>Name</label>
+            <input
+                type='text'
+                name='name'
+                placeholder='Name'
+                value={teamMember.name}
+                onChange={changeHandler}/>
+
+            <label>Email</label>
+            <input
+                type='text'
+                name='email'
+                placeholder='Email'
+                value={teamMember.email}
+                onChange={changeHandler}/>
+
+            <label>Role</label>
+            <input
+                type='text'
+                name='role'
+                placeholder='Role'
+                value={teamMember.role}
+                onChange={changeHandler}/>
+
+            <button type='submit'>Add Team Member</button>
+        </form>
+    )
+}
+
+export default Form;
